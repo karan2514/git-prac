c96303e342a264dbd741429b340866b2bdf9f752
code-with-karan666 <karanthedemon@yahoo.in>
2024-05-13 13:35:59 +0530

    added comment

--- src/main/java/org/git/practice/Main.java
+++ src/main/java/org/git/practice/Main.java
@@ -3,5 +3,8 @@ package org.git.practice;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // added comment to test
+
     }
 }
