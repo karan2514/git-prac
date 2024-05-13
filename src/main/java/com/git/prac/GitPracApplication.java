f78a165436d7eda80162307943d5675bae193738
karan2514 <karan2514singh@gmail.com>
2024-05-13 12:33:03 +0530

    added a comment for demo

---
+++ src/main/java/com/git/prac/GitPracApplication.java
@@ -0,0 +1,15 @@
+package com.git.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracApplication.class, args);
+	}
+
+	// simple comment for practice git
+	
+}
---
+++ src/test/java/com/git/prac/GitPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
